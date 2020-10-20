3c3d537652f603f726ac7354b4ce1ba547aad653
Kao <greenkao@redinfo.app>
2020-10-20 17:14:23 +0800

    10/20

---
+++ WeatherDemo/Model/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  WeatherDemo
+//
+//  Created by redinfo on 2020/10/20.
+//  Copyright © 2020 redinfo. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by redinfo on 2020/10/20.
+//  Copyright © 2020 redinfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
